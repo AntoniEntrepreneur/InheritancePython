200c573abf4e1ba57559d6978f0f2045167cff49
Student <antonipieszczhoch@gmail.com>
2025-01-14 09:31:43 +0100

    Zmodyfikowano funkcję attack

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import random, time
+
 global enemyHP, yourweapons, yourmoney
 enemyHP = 1000
 yourweapons = []
@@ -5,19 +7,50 @@ yourmoney = 10000
 
 class Weapon:
 
-    def __init__(self, name, range, material, damage, cost, weight):
+    def __init__(self, name, range, material, damage, cost, weight, accuracy):
         self.name = name
         self.range = range
         self.material = material
         self.damage = damage
         self.cost = cost
         self.weight = weight
+        self.accuracy = accuracy
 
     def attack(self, strength):
         global enemyHP
         if self.name in yourweapons:
             if strength/10 >= self.weight:
-                enemyHP -= self.damage
+                bodyparts = ["head", "eye", "stomach", "leg", "armor", "missed"]
+                if accuracy= "low":
+                    weights = [5, 0.1, 10, 20, 30, 34.9]
+                elif accuracy= "medium":
+                    weights = [30, 0.5, 40, 5, 20, 4.5]
+                elif accuracy= "high":
+                    weights = [40, 2, 30, 7, 20, 1]
+                bodypart_hit = random.choices(bodyparts, weights = weights, k = 1)[0]
+                if bodypart_hit == "head":
+                    time.sleep(1)
+                    print(f"You hit your enemy in the head")
+                    enemyHP -= self.damage * 10
+                if bodypart_hit == "eye":
+                    time.sleep(1)
+                    print(f"You hit your enemy in the eye")
+                    enemyHP -= self.damage * 100
+                if bodypart_hit == "stomach":
+                    time.sleep(1)
+                    print(f"You hit your enemy in the stomach")
+                    enemyHP -= self.damage * 2
+                if bodypart_hit == "leg":
+                    time.sleep(1)
+                    print(f"You hit your enemy in the head")
+                    enemyHP -= self.damage * 0.5
+                if bodypart_hit == "armor":
+                    time.sleep(1)
+                    print(f"You hit your enemy in the head")
+                    enemyHP -= self.damage * 0.1
+                if bodypart_hit == "missed":
+                    time.sleep(1)
+                    print(f"You hit your enemy in the head")
                 return True
             else:
                 print(f"You are not strong enough to pick up the {self.name}")
@@ -46,8 +79,8 @@ class Weapon:
 
 class Sword(Weapon):
 
-    def __init__(self, name, range, material, damage, cost, weight):
-        super().__init__(name, range, material, damage, cost, weight)
+    def __init__(self, name, range, material, damage, cost, weight, accuracy):
+        super().__init__(name, range, material, damage, cost, weight, accuracy)
 
     def attack(self, strength):
         if super().attack(strength):
@@ -57,16 +90,24 @@ class Sword(Weapon):
 
 class Bow(Weapon):
 
-    def __init__(self, name, range, material, damage, cost, weight):
-        super().__init__(name, range, material, damage, cost, weight)
+    def __init__(self, name, range, material, damage, cost, weight, accuracy):
+        super().__init__(name, range, material, damage, cost, weight, accuracy)
 
     def attack(self, strength):
         if super().attack(strength):
             print(f"You shoot with your {self.name}")
 
+def check_enemyHP():
+    print(f"Enemy's HP is at {enemyHP}")
 
-slasher = Sword("Slasher", 1, "steel", 20, 100, 7)
-apollos_bow = Bow("Apollo's Bow", 200, "wood", 10, 200, 4)
+check_enemyHP()
+
+slasher = Sword("Slasher", 1, "steel", 20, 100, 7, 95)
+apollos_bow = Bow("Apollo's Bow", 200, "wood", 10, 200, 4, 50)
 
 apollos_bow.buy()
 apollos_bow.attack(1000)
+
+
+
+check_enemyHP()
