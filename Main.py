0b2d3ecf08039e06731f7fa6dbf4a89bae34c7d1
Student <antonipieszczhoch@gmail.com>
2025-01-14 08:48:56 +0100

    Dodano konstruktor

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
-print("Jajo")
+class Weapon:
+
+    def __init__(self, range, material, damage, cost, weight):
+        self.range = range
+        self.material = material
+        self.damage = damage
+        self.cost = cost
