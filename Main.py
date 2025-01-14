dd9c3034500ef9ecde1e6d4b8d2ec6ef9e8e45e4
Student <antonipieszczhoch@gmail.com>
2025-01-14 09:17:35 +0100

    Zdefiniowano klasę BOW

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+global enemyHP, yourweapons, yourmoney
 enemyHP = 1000
 yourweapons = []
 yourmoney = 10000
@@ -12,14 +13,25 @@ class Weapon:
         self.cost = cost
         self.weight = weight
 
-    def attack(self):
-        pass
+    def attack(self, strength):
+        global enemyHP
+        if self.name in yourweapons:
+            if strength/10 >= self.weight:
+                enemyHP -= self.damage
+                return True
+            else:
+                print(f"You are not strong enough to pick up the {self.name}")
+            return False
+        else:
+            print("You don't have that weapon.")
+            return False
 
     def buy(self):
+        global yourmoney, yourweapons
         if yourmoney >= self.cost:
             yourmoney -= self.cost
             print(f"You buy a {self.name} for ${self.cost}")
-            yourweapons.append(self)
+            yourweapons.append(self.name)
 
     def examine(self):
         parameters = {
@@ -37,8 +49,24 @@ class Sword(Weapon):
     def __init__(self, name, range, material, damage, cost, weight):
         super().__init__(name, range, material, damage, cost, weight)
 
-    def attack(self):
-        print(f"You slash with your {self.name}")
-        enemy
+    def attack(self, strength):
+        if super().attack(strength):
+            print(f"You slash with your {self.name}")
 
-slasher = Sword("Slasher", 5, "wood", 5, 100, 7)
+
+
+class Bow(Weapon):
+
+    def __init__(self, name, range, material, damage, cost, weight):
+        super().__init__(name, range, material, damage, cost, weight)
+
+    def attack(self, strength):
+        if super().attack(strength):
+            print(f"You shoot with your {self.name}")
+
+
+slasher = Sword("Slasher", 1, "steel", 20, 100, 7)
+apollos_bow = Bow("Apollo's Bow", 200, "wood", 10, 200, 4)
+
+apollos_bow.buy()
+apollos_bow.attack(1000)
